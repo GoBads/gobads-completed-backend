dae3bf7d0ddcc4c6f6a969c48ce8b2ac7d443d62
VictorLimaRt <revoltcraft432@gmail.com>
2022-07-22 22:05:46 -0300

    Updating cors policy

--- src/controllers/GetAllPlayersController.ts
+++ src/controllers/GetAllPlayersController.ts
@@ -4,6 +4,8 @@ import { GetAllPlayersService } from "../services/GetAllPlayersService";
 class GetAllPlayersController {
     async handle(request: Request, response: Response) {
 
+        response.setHeader('Access-Control-Allow-Origin', 'https://gobads.netlify.app');
+
         const service = new GetAllPlayersService();
         try {
             const players = await service.execute();
