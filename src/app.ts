7dd62fd45b32a233bf9bc477743b5de327ce5b75
VictorLimaRt <revoltcraft432@gmail.com>
2022-07-22 22:02:34 -0300

    Updating cors policy

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,13 @@ import { router } from "./routes";
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://gobads.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+    });
+
 const serverHttp = http.createServer(app);
 
 app.use(express.json());
